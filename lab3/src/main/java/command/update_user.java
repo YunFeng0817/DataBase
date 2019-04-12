0ba3192d1b45ac0d2a41d09bfa10a3eaffd33f5c
Mark <f18846188605@gmail.com>
2019-04-13 02:55:01 +0800

    working

--- lab3/src/main/java/command/update_user.java
+++ lab3/src/main/java/command/update_user.java
@@ -16,44 +16,26 @@ public class update_user extends command {
     @Override
     public void run(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
-        String sql = "insert into user(";
+        String sql = "update user set ";
         String items = "";
-        String values = "";
         if (user.getPassword() != null) {
-            items += (items.equals("") ? "" : ",") + "password";
-            values += (values.equals("") ? "" : ",") + "'" + user.getPassword() + "'";
+            items += (items.equals("") ? "" : ",") + "password=" + "'" + user.getPassword() + "'";
         }
         if (user.getName() != null) {
-            items += (items.equals("") ? "" : ",") + "name";
-            values += (values.equals("") ? "" : ",") + "'" + user.getName() + "'";
+            items += (items.equals("") ? "" : ",") + "name=" + "'" + user.getName() + "'";
         }
         if (user.getBirthday() != null) {
-            items += (items.equals("") ? "" : ",") + "birthday";
-            values += (values.equals("") ? "" : ",") + "'" + user.getBirthday() + "'";
+            items += (items.equals("") ? "" : ",") + "birthday=" + "'" + user.getBirthday() + "'";
         }
         if (user.getGender() != null) {
-            items += (items.equals("") ? "" : ",") + "gender";
-            values += (values.equals("") ? "" : ",") + "'" + user.getGender() + "'";
+            items += (items.equals("") ? "" : ",") + "gender=" + "'" + user.getGender() + "'";
         }
         if (user.getAddress() != null) {
-            items += (items.equals("") ? "" : ",") + "address";
-            values += (values.equals("") ? "" : ",") + "'" + user.getAddress() + "'";
+            items += (items.equals("") ? "" : ",") + "address=" + "'" + user.getAddress() + "'";
         }
-        sql = sql + items + ") values(" + values + ");";
+        if (!items.equals(""))
+            sql = sql + items + "where user_id=" + user.getUserID() + ";";
         statement.execute(sql);
-        sql = "select user_id from user where name='" + user.getName() + "';";
-        ResultSet resultSet = statement.executeQuery(sql);
-        resultSet.next();
-        int user_id = resultSet.getInt("user_id");
-        System.out.println("Your user id : " + user_id);
-        System.out.println("Your user name : " + user.getName());
-        if (user.getEmails().size() > 0) {
-            for (String email : user.getEmails()) {
-                sql = "insert into email(user_id,address) values(" + user_id + ",'" + email + "');";
-                statement.execute(sql);
-            }
-        }
-        resultSet.close();
         statement.close();
     }
 
