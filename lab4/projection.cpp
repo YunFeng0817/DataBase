ba11c5662e9deab7fb2db3e03670052fd9325542
Mark <f18846188605@gmail.com>
2019-05-12 12:23:19 +0800

    finish projection

---
+++ lab4/projection.cpp
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include "extmem.h"
+#include "BplusTree.h"
+using namespace std;
+
+const int R_root = 132, S_root = 197;
+
+int main()
+{
+    Buffer buf; /* A buffer */
+    /* Initialize the buffer */
+    if (!initBuffer(520, 64, &buf))
+    {
+        perror("Buffer Initialization Failed!\n");
+        return -1;
+    }
+
+    valueNode *blk = NULL; /* A pointer to a block */
+    int *int_blk = NULL;
+    int length;
+    createTree(R_root, &buf);
+    leaf_list *leaves;
+    leaves = commandGetResult();
+    writeAll();
+    int size = leaves->size;
+    cout << "Projection result: " << endl;
+    cout << "A" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << leaves->leaves[i].key << endl;
+    }
+}
